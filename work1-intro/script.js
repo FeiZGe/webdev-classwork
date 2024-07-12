c3d9b260f2b49f67da53a6c222ec82d6b5b9a851
FeiZGe <thanapattt.m@gmail.com>
2024-07-12 18:03:15 +0700

    del responsive navbar

--- work1-intro/script.js
+++
@@ -1,7 +0,0 @@
-const menuIcon = document.querySelector('#menu-icon');
-const menu = document.querySelector('.menu');
-
-menuIcon.onclick = () => {
-    menuIcon.classList.toggle('op-menu');
-    menu.classList.toggle('open');
-}
